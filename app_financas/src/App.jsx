3b18ec7cc80b7fdd77780e31f183f8eaaa7ff961
Lucas De Lucena Limeira <lucasdllimeira@hotmail.com>
2025-01-03 14:31:15 -0300

    updates

---
+++ app_financas/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Financas from './components/title'
+
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid py-4 min-h-screen'>
+      <Financas/>
+    </div>
+  )
+}
+
+export default App
---
+++ app_financas/src/components/title.jsx
@@ -0,0 +1,33 @@
+
+
+
+const Financas = () => {
+
+
+  return (
+    <div 
+    className='bg-white items-center place-self-center w-11/12 max-w-6xl flex flex-col p-7 min-h-[700px] rounded-xl'>
+
+{/* --------- title ----------*/}
+
+        <div 
+            className='flex items-center gap-2'>
+                
+                <h1 
+                    className=' text-3xl font-semibold uppercase'>
+                        Controle financeiro
+                </h1>
+        </div>
+
+{/* --------- Input box ----------*/}
+        
+
+{/* --------- Todo list ----------*/}
+
+        
+
+    </div>
+  )
+}
+
+export default Financas
